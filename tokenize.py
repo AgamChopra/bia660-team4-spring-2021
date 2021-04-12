e781feb83387f9c922068db3069d28356e8c6bfc
Agam main <work.agamchopra@gmail.com>
2021-04-11 20:52:02 -0400

    simplified tokenize.py

--- tokenize.py
+++ tokenize.py
@@ -4,24 +4,18 @@ import pandas as pd
 import nltk
 from nltk.corpus import stopwords
 
-def load_data(file_path):
-    df = pd.read_csv (r'%s'%(file_path))
-    data = df.values
-    data = data[:,1:]
-    return data
+def load_data(file_path):  
+    return pd.read_csv (r'%s'%(file_path)).values[:,1:]
     
 def processed_data(data):
-    paper_titles = data[:,0]
-    paper_texts = data[:,1]
-    paper_texts = [sents.lower() for sents in paper_texts]
-    return paper_titles, paper_texts
+    return data[:,0], [sents.lower() for sents in data[:,1]]
 
 def tokenize_sen(sen):
     stop_words = set(stopwords.words("english"))
-    tokens = [word for word in [''.join(e for e in string if e.isalnum()) for string in [x.replace('\\n','') for x in nltk.word_tokenize(sen)]] if word not in stop_words and word != '']
-    return tokens
+    return [word for word in [''.join(e for e in string if e.isalnum()) for string in [x.replace('\\n','') for x in nltk.word_tokenize(sen)]] if word not in stop_words and word != '']
 
 '''
-_,test = processed_data(load_data(file_path = "ICO1_filtered.csv"))
-print(tokenize_sen(test[0]))
+title,text = processed_data(load_data(file_path = "ICO1_filtered.csv"))
+print(tokenize_sen(text[0]))
+print(title[0])
 '''
