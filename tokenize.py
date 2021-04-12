da05ced5d42636585d1997305c30b47d8f5792ce
Agam main <work.agamchopra@gmail.com>
2021-04-11 21:25:50 -0400

    added tf_idf function to tokenize.py

--- tokenize.py
+++ tokenize.py
@@ -14,8 +14,28 @@ def tokenize_sen(sen):
     stop_words = set(stopwords.words("english"))
     return [word for word in [''.join(e for e in string if e.isalnum()) for string in [x.replace('\\n','') for x in nltk.word_tokenize(sen)]] if word not in stop_words and word != '']
 
+def token_count(sen):
+    return {token:sen.count(token) for token in set(sen)}
+
+def tf_idf(docs):
+    docs_tokens={idx:token_count(doc) for idx,doc in enumerate(docs)}
+    dtm=pd.DataFrame.from_dict(docs_tokens, orient="index" )
+    dtm=dtm.fillna(0)
+    dtm = dtm.sort_index(axis = 0)        
+    tf=dtm.values
+    doc_len=tf.sum(axis=1, keepdims=True)
+    tf=np.divide(tf, doc_len)
+    df=np.where(tf>0,1,0)
+    smoothed_idf=np.log(np.divide(len(docs)+1, np.sum(df, axis=0)+1))+1    
+    smoothed_tf_idf=tf*smoothed_idf
+    return smoothed_tf_idf
+
 '''
 title,text = processed_data(load_data(file_path = "ICO1_filtered.csv"))
 print(tokenize_sen(text[0]))
 print(title[0])
+title,text = processed_data(load_data(file_path = "D:\\main\\Education\\Stevens MBA (2020-2022)\\1_Spring 2021\\BIA 660\\Project\\Datasets\\ICO1_filtered.csv"))
+tok_sens = [tokenize_sen(sen) for sen in text]
+tfidf = tf_idf(tok_sens)
+print(tfidf)
 '''
